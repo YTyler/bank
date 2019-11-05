022ad2b2c22a47398de9862927f04d4580b02c64
Student <example@example.com>
2019-11-05 14:31:48 -0800

    add Directory class, untested

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,45 @@
-// var bankAccounts = [];
-var newAccount;
+//Bank Directory Business Logic
+function Directory(){
+  this.accounts = [],
+  this.currentId = 0
 
-function BankAccount(initialAmount = 0){
+}
 
-  this.initialAmount = initialAmount;
-  this.balance = initialAmount;
+Directory.prototype.assignId = function (){
+  this.currentId += 1;
+  return this.currentId;
+
+}
+
+Directory.prototype.addAccount = function(account){
+  account.id = this.assignId();
+  this.accounts.push(account);
+
+}
+Directory.prototype.findAccount = function(searchId) {
+  for (var i = 0; i < this.accounts.length; i++){
+    if (this.accounts[i]) {
+      if (this.accounts[i].id === searchId) {
+        return this.accounts[i];
+      }
+    }
+  }
+}
+Directory.prototype.deleteAccount = function(searchId){
+  var toDelete = findAccount(searchId);
+  delete toDelete;
+}
+
+
+//Bank Account Business Logic
+function BankAccount(amount = 0){
+
+  this.balance = amount;
 
 };
-
 BankAccount.prototype.deposit = function (amount){
   this.balance += amount;
 };
-
 BankAccount.prototype.withdrawal = function (amount){
   this.balance -= amount;
 };
@@ -19,6 +47,9 @@ BankAccount.prototype.withdrawal = function (amount){
 
 
 $(document).ready(function(){
+
+var newAccount;
+
   $("#fundForm").submit(function(event){
     event.preventDefault();
 
